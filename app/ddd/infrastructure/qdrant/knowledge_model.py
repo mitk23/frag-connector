e04ebd547d18bee276ec1c0c312cc0d6a08d648a
mitk23 <mitk232@gmail.com>
2024-12-04 19:33:21 +0900

    feat: add param to make retrieval exact (= enable/disable ANN)

--- app/ddd/infrastructure/qdrant/knowledge_model.py
+++ app/ddd/infrastructure/qdrant/knowledge_model.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from ddd.domains.knowledge import Knowledge, KnowledgeQuery
 from pydantic import BaseModel
-from qdrant_client.models import Filter
+from qdrant_client.models import Filter, SearchParams
 
 
 class KnowledgeQdrantDao(BaseModel):
@@ -24,6 +24,7 @@ class KnowledgeQueryQdrantDao(BaseModel):
     with_vectors: bool | None = True
     with_payload: bool | None = True
     query_filter: Filter | None = None
+    search_params: SearchParams | None = None
 
     @staticmethod
     def from_entity(query: KnowledgeQuery) -> "KnowledgeQueryQdrantDao":
@@ -33,4 +34,5 @@ class KnowledgeQueryQdrantDao(BaseModel):
             with_vectors=query.config.include_embedding,
             with_payload=query.config.include_metadata,
             query_filter=Filter.model_validate(query.config.filter) if query.config.filter else None,
+            search_params=SearchParams(exact=query.config.exact_search),
         )
